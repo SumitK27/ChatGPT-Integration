40520a3b441840f34e08e4fa9fb615f397e6295b
SumitK27 <sumitkajbaje@gmail.com>
2023-04-16 16:04:26 +0530

    Added Express

---
+++ README.md
@@ -0,0 +1,15 @@
+# _Chat GPT API Integration_
+
+This is a simple integration of the [Chat GPT](https://chatgpt.openai.com/) in a NodeJS application.
+
+## Installation
+
+```bash
+npm install
+```
+
+## Usage
+
+1. Rename the `.env.example` file to `.env` and fill in the `ORGANIZATION`, `API_KEY` and `PORT` variables.
+2. Run the application with `npm start`.
+3. Open the browser and go to `http://localhost:3000/` to see the application running.
--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 import dotenv from "dotenv";
+
 dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
 
 const configuration = new Configuration({
     organization: process.env.ORGANIZATION,
     apiKey: process.env.API_KEY,
 });
-
 const openai = new OpenAIApi(configuration);
 
-const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-        {
-            role: "user",
-            content: "Hello World",
-        },
-    ],
+app.get("/", async (req, res) => {
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: "user",
+                content: "Hello World",
+            },
+        ],
+    });
+
+    res.json(completion.data.choices[0].message);
 });
 
-console.log(completion.data.choices[0].message);
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
